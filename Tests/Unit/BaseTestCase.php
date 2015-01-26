dd6ea34df81afe6fd25a807f873feea5450496e7
Tim Lochmüller <tim@fruit-lab.de>
2015-01-26 16:10:00 +0100

    CI Test commit
    CI Test commit

---
+++ Tests/Unit/BaseTestCase.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * First base Test
+ *
+ * @package Hdnet
+ * @author  Tim Lochmüller
+ */
+
+namespace HDNET\Calendarize\Tests;
+
+/**
+ * First base Test
+ *
+ * @author Tim Lochmüller
+ */
+abstract class BaseTestCase extends \PHPUnit_Framework_TestCase {
+
+}
