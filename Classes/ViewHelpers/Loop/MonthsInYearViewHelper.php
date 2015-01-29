ee74db967d3ae440b9857f1da6bd76d1b1bdc1c3
Tim Lochmüller <tim@fruit-lab.de>
2015-01-29 14:55:13 +0100

    Year view
    Year view

---
+++ Classes/ViewHelpers/Loop/MonthsInYearViewHelper.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Months in year view Helper
+ *
+ * @package Calendarize\ViewHelpers\Loop
+ * @author  Tim Lochmüller
+ */
+
+namespace HDNET\Calendarize\ViewHelpers\Loop;
+
+/**
+ * Months in year view Helper
+ *
+ * @author Tim Lochmüller
+ */
+class MonthsInYearViewHelper extends AbstractLoopViewHelper {
+
+	/**
+	 * @param \DateTime $date
+	 *
+	 * @return array
+	 */
+	protected function getItems(\DateTime $date) {
+		$months = array();
+		$date->setDate($date->format('Y'), $date->format('n'), 1);
+		for ($i = 0; $i < 12; $i++) {
+			$months[$date->format('n')] = array(
+				'week'   => $date->format('n'),
+				'date'   => clone $date,
+				'break3' => $date->format('n') % 3,
+				'break4' => $date->format('n') % 4,
+			);
+			$date->modify('+1 month');
+		}
+		return $months;
+	}
+}
