729fd7a5a1b00e1ea98c5262d60fec399a13edb4
Tim Lochmüller <tim@fruit-lab.de>
2015-02-19 16:03:59 +0100

    Implement days in month helper

--- Classes/ViewHelpers/Loop/DaysInMonthViewHelper.php
+++ Classes/ViewHelpers/Loop/DaysInMonthViewHelper.php
@@ -23,10 +23,19 @@ class DaysInMonthViewHelper extends AbstractLoopViewHelper {
 	 * @return array
 	 */
 	protected function getItems(\DateTime $date) {
-		$weeks = array();
+		$daysInMonth = $date->format('t');
+		$days = array();
+		$move = (int)($date->format('j') - 1);
+		$date->modify('-' . $move . ' days');
 
-		// @todo implement
+		for ($i = 0; $i < $daysInMonth; $i++) {
+			$days[] = array(
+				'day'  => $i,
+				'date' => clone $date,
+			);
+			$date->modify('+1 day');
+		}
+		return $days;
 
-		return $weeks;
 	}
 }
