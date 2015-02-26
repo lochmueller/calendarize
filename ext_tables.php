c8fd10df75cbc5c7cd635f1eb6ff8fd176180b9d
Tim Lochmüller <tim@fruit-lab.de>
2015-02-26 09:09:18 +0100

    Remove typo3conf-path in extension
    Remove typo3conf-path in extension

--- ext_tables.php
+++ ext_tables.php
@@ -23,7 +23,8 @@ $pluginName = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('pluginN
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('calendarize', 'Calendar', $pluginName);
 
 // module icon
-\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon('pages', 'contains-calendar', '../typo3conf/ext/calendarize/ext_icon.png');
+$relIconPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('calendarize') . 'ext_icon.png';
+\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon('pages', 'contains-calendar', $relIconPath);
 $addCalendarizeToModuleSelection = TRUE;
 foreach ($GLOBALS['TCA']['pages']['columns']['module']['config']['items'] as $item) {
 	if ($item[1] === 'calendar') {
@@ -35,6 +36,6 @@ if ($addCalendarizeToModuleSelection) {
 	$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = array(
 		0 => 'Calendarize',
 		1 => 'calendar',
-		2 => '../typo3conf/ext/calendarize/ext_icon.png'
+		2 => $relIconPath
 	);
 }
