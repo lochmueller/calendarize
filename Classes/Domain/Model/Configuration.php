ef0daa4958ba568a0bd68653a73af1e0dd14192f
Tim Lochmüller <tim@fruit-lab.de>
2015-02-27 13:54:18 +0100

    Move the constants to the interface
    Move the constants to the interface

--- Classes/Domain/Model/Configuration.php
+++ Classes/Domain/Model/Configuration.php
@@ -15,59 +15,7 @@ namespace HDNET\Calendarize\Domain\Model;
  * @db
  * @smartExclude language,enableFields
  */
-class Configuration extends AbstractModel {
-
-	const TYPE_TIME = 'time';
-
-	const TYPE_INCLUDE_GROUP = 'include';
-
-	const TYPE_EXCLUDE_GROUP = 'exclude';
-
-	const TYPE_EXTERNAL = 'external';
-
-	const FREQUENCY_NONE = '';
-
-	const FREQUENCY_DAILY = 'daily';
-
-	const FREQUENCY_WEEKLY = 'weekly';
-
-	const FREQUENCY_MONTHLY = 'monthly';
-
-	const FREQUENCY_YEARLY = 'yearly';
-
-	const DAY_NONE = '';
-
-	const DAY_SPECIAL_WEEKDAY = 'weekday';
-
-	const DAY_SPECIAL_WORKDAY = 'workday';
-
-	const DAY_SPECIAL_BUSINESS = 'business';
-
-	const DAY_MONDAY = 'monday';
-
-	const DAY_TUESDAY = 'tuesday';
-
-	const DAY_WEDNESDAY = 'wednesday';
-
-	const DAY_THURSDAY = 'thursday';
-
-	const DAY_FRIDAY = 'friday';
-
-	const DAY_SATURDAY = 'saturday';
-
-	const DAY_SUNDAY = 'sunday';
-
-	const RECURRENCE_NONE = '';
-
-	const RECURRENCE_FIRST = 'first';
-
-	const RECURRENCE_SECOND = 'second';
-
-	const RECURRENCE_THIRD = 'third';
-
-	const RECURRENCE_FOURTH = 'fourth';
-
-	const RECURRENCE_LAST = 'last';
+class Configuration extends AbstractModel implements ConfigurationInterface {
 
 	/**
 	 * Type
