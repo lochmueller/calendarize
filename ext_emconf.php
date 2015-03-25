806139aa7a7430130008eed1dc30b9aa5b0af19a
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-03-25 08:46:43 +0100

    Raise compatible version and set current Release version to 1.3.1
    Raise compatible version and set current Release version to 1.3.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,14 +12,14 @@ $EM_CONF['calendarize'] = array(
 	'title'            => 'Calendarize',
 	'description'      => 'Create a structure for timely controlled tables and one plugin for the different output of calendar views. The extension is shipped with one default event table, but the aim of the extension is to "calendarize" every table/model. It is completely independent and configurable! Use your own models as event items in this calender. We need feedback about the concept! Development on https://github.com/lochmueller/calendarize',
 	'category'         => 'fe',
-	'version'          => '1.3.0',
+	'version'          => '1.3.1',
 	'state'            => 'beta',
 	'clearcacheonload' => 1,
 	'author'           => 'Tim Lochmüller',
 	'author_email'     => 'tim@fruit-lab.de',
 	'constraints'      => array(
 		'depends' => array(
-			'typo3'      => '6.2.0-7.1.99',
+			'typo3'      => '6.2.0-7.2.99',
 			'autoloader' => '1.5.5-0.0.0',
 		),
 	),
