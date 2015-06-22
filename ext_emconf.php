894791699b8c5d2478a1974d8d6026f703775b9a
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-06-22 15:15:03 +0200

    Release for new TYPO3 Version and also update the autoloader

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,15 +12,15 @@ $EM_CONF['calendarize'] = array(
 	'title'            => 'Calendarize',
 	'description'      => 'Create a structure for timely controlled tables and one plugin for the different output of calendar views. The extension is shipped with one default event table, but the aim of the extension is to "calendarize" every table/model. It is completely independent and configurable! Use your own models as event items in this calender. We need feedback about the concept! Development on https://github.com/lochmueller/calendarize',
 	'category'         => 'fe',
-	'version'          => '1.4.0',
+	'version'          => '1.4.1',
 	'state'            => 'stable',
 	'clearcacheonload' => 1,
 	'author'           => 'Tim Lochmüller',
 	'author_email'     => 'tim@fruit-lab.de',
 	'constraints'      => array(
 		'depends' => array(
-			'typo3'      => '6.2.0-7.2.99',
-			'autoloader' => '1.5.5-0.0.0',
+			'typo3'      => '6.2.0-7.99.99',
+			'autoloader' => '1.6.0-0.0.0',
 		),
 	),
 );
