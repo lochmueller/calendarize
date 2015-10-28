f3caced34d0ff2594572e01b041d71e57341333b
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-10-28 09:18:06 +0100

    Remove console.log in the JS

--- Resources/Public/JavaScript/MomentReplacement.js
+++ Resources/Public/JavaScript/MomentReplacement.js
@@ -1,11 +1,10 @@
 if (window.jQuery) {
-	jQuery(function () {
-		jQuery('.momentJs').each(function () {
-			var m = moment(jQuery(this).html());
-			console.log(m);
-			jQuery(this).html(m.format(jQuery(this).attr('data-format')));
-		});
-	});
+    jQuery(function () {
+        jQuery('.momentJs').each(function () {
+            var m = moment(jQuery(this).html());
+            jQuery(this).html(m.format(jQuery(this).attr('data-format')));
+        });
+    });
 } else {
-	alert('The Moment.js function of the calendarize need jQuery!');
+    alert('The Moment.js function of the calendarize need jQuery!');
 }
