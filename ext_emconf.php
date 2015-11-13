00643015972070272390a86a28ab07dd9c9b4b5f
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-11-13 09:03:13 +0100

    Fix autoloader dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF['calendarize'] = array(
         'depends' => array(
             'typo3'      => '6.2.0-7.99.99',
             'php'        => '5.4.0-0.0.0',
-            'autoloader' => '1.9.2-0.0.0',
+            'autoloader' => '1.10.0-0.0.0',
         ),
     ),
 );
