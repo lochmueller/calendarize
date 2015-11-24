da19809878b201ee3084261085189f01f9870e8f
Alexander Grein <mail@typo3-webagentur.com>
2015-11-24 15:07:27 +0100

    Use $_EXTKEY instead of hard codes extkey to fix problem with composer installation under TYPO3 7 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@
  * @author   Tim Lochmüller
  */
 
-$EM_CONF['calendarize'] = array(
+$EM_CONF[$_EXTKEY] = array(
     'title'            => 'Calendarize',
     'description'      => 'Create a structure for timely controlled tables and one plugin for the different output of calendar views. The extension is shipped with one default event table, but the aim of the extension is to "calendarize" every table/model. It is completely independent and configurable! Use your own models as event items in this calender. We need feedback about the concept! Development on https://github.com/lochmueller/calendarize',
     'category'         => 'fe',
