48b3fb5227bda8dcca6988f2974a153b8c8ff48e
Tim Lochmüller <tim@fruit-lab.de>
2016-01-07 19:31:54 +0100

    [BUGFIX] Resepct open end in momentJs context and set momentJs to UTC

--- Resources/Public/JavaScript/MomentReplacement.js
+++ Resources/Public/JavaScript/MomentReplacement.js
@@ -1,10 +1,11 @@
 if (window.jQuery) {
-    jQuery(function () {
-        jQuery('.momentJs').each(function () {
-            var m = moment(jQuery(this).html());
-            jQuery(this).html(m.format(jQuery(this).attr('data-format')));
-        });
-    });
+	jQuery(function () {
+		jQuery('.momentJs').each(function () {
+			var $element = jQuery(this);
+			var m = moment.utc($element.html());
+			jQuery(this).html(m.format($element.attr('data-format')));
+		});
+	});
 } else {
-    alert('The Moment.js function of the calendarize need jQuery!');
+	alert('The Moment.js function of the calendarize need jQuery!');
 }
