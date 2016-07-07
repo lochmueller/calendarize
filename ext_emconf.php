3e11ae1b8ec65d1754de0847d8e88201d88682c4
Tim Lochmüller <tim@fruit-lab.de>
2016-07-07 11:17:44 +0200

    Bump version to 2.3.2 and add 8.2.x compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,14 +11,14 @@ $EM_CONF[$_EXTKEY] = [
     'title'            => 'Calendarize - Event Management',
     'description'      => 'Create a structure for timely controlled tables (e.g. events) and one plugin for the different output of calendar views (list, detail, month, year, day, week...). The extension is shipped with one default event table, but you can also "calendarize" your own table/model. It is completely independent and configurable! Use your own models as event items in this calender. Development on https://github.com/lochmueller/calendarize',
     'category'         => 'fe',
-    'version'          => '2.3.1',
+    'version'          => '2.3.2',
     'state'            => 'stable',
     'clearcacheonload' => 1,
     'author'           => 'Tim Lochmüller',
     'author_email'     => 'tim@fruit-lab.de',
     'constraints'      => [
         'depends' => [
-            'typo3'      => '6.2.0-8.1.99',
+            'typo3'      => '6.2.0-8.2.99',
             'php'        => '5.5.0-0.0.0',
             'autoloader' => '2.0.0-0.0.0',
         ],
