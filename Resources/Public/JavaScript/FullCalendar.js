bf230ef7caa02b7f6fcc951bd161152c8c7a6a31
Tim Lochmüller <tim@fruit-lab.de>
2016-12-04 19:00:15 +0100

    Update FullCalendar IO to 3.0.1, set firstDay by configuration, fix allDay events in FullCalendar IO

--- Resources/Public/JavaScript/FullCalendar.js
+++ Resources/Public/JavaScript/FullCalendar.js
@@ -2,15 +2,19 @@ if (window.jQuery) {
 	jQuery(function () {
 		var $monthCalendar = jQuery('.fullCalendarIo');
 		if ($monthCalendar.length && typeof monthEvents != 'undefined') {
+			monthFirstDay = parseInt(monthFirstDay, 10);
+			if (monthFirstDay == 7) {
+				monthFirstDay = 0;
+			}
 
 			var language = $monthCalendar.first().attr('data-language');
 			if (typeof language == typeof undefined || language == false) {
 				language = "en";
 			}
-			
+
 			jQuery('.regularMonthView').hide();
 			$monthCalendar.fullCalendar({
-				lang: language, header: {
+				lang: language, firstDay: monthFirstDay, header: {
 					left: '', center: '', right: ''
 				}, defaultDate: monthEventsCurrentDate, events: monthEvents
 			});
