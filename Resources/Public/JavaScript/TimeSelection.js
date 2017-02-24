59a4b2f6b4bc11e2f5ca959b2f64cdd223cc87b7
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-02-24 09:28:41 +0100

    Fix #153 - Add time selection wirzard for 8.x

--- Resources/Public/JavaScript/TimeSelection.js
+++ Resources/Public/JavaScript/TimeSelection.js
@@ -1,9 +1,18 @@
 define(['jquery'], function ($) {
-    $('.calendarize-time-selection').on("change", function () {
-        var currentField = $(this);
-        var originalField = $('#' + currentField.data('related'));
-        originalField.val(currentField.val());
-        originalField.trigger('change');
-        currentField.val('');
-    });
+	$('.calendarize-time-selection').on("change", function () {
+		var currentField = $(this);
+		var name = currentField.data('related-name');
+		var id = currentField.data('related-id');
+		var originalField = null;
+		if (name.length) {
+			originalField = $("input[name='" + name + "']");
+		} else if (id.length) {
+			originalField = $("#" + id);
+		} else {
+			return originalField;
+		}
+		originalField.val(currentField.val());
+		originalField.trigger('change');
+		currentField.val('');
+	});
 });
