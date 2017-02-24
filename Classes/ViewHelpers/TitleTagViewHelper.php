23b1c3891ba6975730e3917b5ceab86e5708f631
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-02-24 08:18:06 +0100

    Fix #139 - [FEATURE] Add page title on detail pages

---
+++ Classes/ViewHelpers/TitleTagViewHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * TitleTagViewHelper
+ */
+
+namespace HDNET\Calendarize\ViewHelpers;
+
+/**
+ * TitleTagViewHelper
+ * 
+ * @see https://github.com/georgringer/news/blob/master/Classes/ViewHelpers/TitleTagViewHelper.php
+ */
+class TitleTagViewHelper extends AbstractViewHelper
+{
+
+    /**
+     * Render
+     */
+    public function render()
+    {
+        $content = trim($this->renderChildren());
+        if (!empty($content)) {
+            $GLOBALS['TSFE']->altPageTitle = $content;
+            $GLOBALS['TSFE']->indexedDocTitle = $content;
+        }
+    }
+}
