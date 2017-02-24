6b4c667cea6c8895080f433eba1c29fcc5d68ec6
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-02-24 09:27:49 +0100

    Add EXT:microformat extension integration

---
+++ Classes/ViewHelpers/IfExtensionLoadedViewHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * IfExtensionLoadedViewHelper
+ */
+
+namespace HDNET\Calendarize\ViewHelpers;
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
+
+/**
+ * IfExtensionLoadedViewHelper
+ */
+class IfExtensionLoadedViewHelper extends AbstractConditionViewHelper
+{
+    /**
+     * Init arguments
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('extensionKey', 'string', 'The extension key of the extension', true);
+    }
+
+    /**
+     * Add the condition
+     *
+     * @param array|null $arguments
+     * @return bool
+     */
+    public static function evaluateCondition($arguments = null)
+    {
+        if (!isset($arguments['extensionKey'])) {
+            return false;
+        }
+        return ExtensionManagementUtility::isLoaded($arguments['extensionKey']);
+    }
+
+
+}
