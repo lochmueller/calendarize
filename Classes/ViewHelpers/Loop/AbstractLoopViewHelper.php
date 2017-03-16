edad396b565e4e7c4e6e98debb9b01d5dc89d0db
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-03-16 15:51:31 +0100

    Avoid the escaping of the loop ViewHelper

--- Classes/ViewHelpers/Loop/AbstractLoopViewHelper.php
+++ Classes/ViewHelpers/Loop/AbstractLoopViewHelper.php
@@ -17,6 +17,27 @@ use HDNET\Calendarize\ViewHelpers\AbstractViewHelper;
 abstract class AbstractLoopViewHelper extends AbstractViewHelper
 {
 
+
+    /**
+     * Specifies whether the escaping interceptors should be disabled or enabled for the result of renderChildren() calls within this ViewHelper
+     * @see isChildrenEscapingEnabled()
+     *
+     * Note: If this is NULL the value of $this->escapingInterceptorEnabled is considered for backwards compatibility
+     *
+     * @var boolean
+     * @api
+     */
+    protected $escapeChildren = false;
+
+    /**
+     * Specifies whether the escaping interceptors should be disabled or enabled for the render-result of this ViewHelper
+     * @see isOutputEscapingEnabled()
+     *
+     * @var boolean
+     * @api
+     */
+    protected $escapeOutput = false;
+
     /**
      * Render the element
      *
