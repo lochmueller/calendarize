cb41b9f0f37fc2e9b472b81096f80f38c04ed2d2
Tim Lochmüller <tim@fruit-lab.de>
2018-10-08 21:28:27 +0200

    [FEATURE] Add help method to get the next inde for the event

--- Classes/Domain/Repository/EventRepository.php
+++ Classes/Domain/Repository/EventRepository.php
@@ -7,7 +7,11 @@ declare(strict_types=1);
 
 namespace HDNET\Calendarize\Domain\Repository;
 
+use HDNET\Calendarize\Domain\Model\Event;
+use HDNET\Calendarize\Domain\Model\Index;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Persistence\Generic\Query;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 
 /**
  * Event repository.
@@ -52,5 +56,44 @@ class EventRepository extends AbstractRepository
                 return $source->getSelectorName();
             }
         }
+    }
+
+    /**
+     * Get the right Index ID by the event ID
+     *
+     * @param int $uid
+     *
+     * @return Index|null
+     */
+    protected function findNextIndex(int $uid)
+    {
+        /** @var Event $event */
+        $event = $this->findByUid($uid);
+
+        if (!is_object($event)) {
+            return;
+        }
+
+        $objectManager = new ObjectManager();
+        /** @var IndexRepository $indexRepository */
+        $indexRepository = $objectManager->get(IndexRepository::class);
+
+        try {
+            $result = $indexRepository->findByEventTraversing($event, true, false, 1, QueryInterface::ORDER_ASCENDING);
+            if (empty($result)) {
+                $result = $indexRepository->findByEventTraversing($event, false, true, 1, QueryInterface::ORDER_DESCENDING);
+            }
+        } catch (\Exception $ex) {
+            return;
+
+        }
+
+        if (empty($result)) {
+            return;
+        }
+
+        /** @var Index $index */
+        $index = $result[0];
+        return $index;
     }
 }
