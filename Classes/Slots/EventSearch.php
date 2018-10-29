lochmueller/calendarize

commit 68f90b8329015bcbc69ba30e946545a0558e6187

Some code cleanups/fixes

Classes/Slots/EventSearch.php
@@ -48,8 +48,8 @@ class EventSearch
      */
     public function setIdsByCustomSearch(
         array $indexIds,
-        \DateTime $startDate = null,
-        \DateTime $endDate = null,
+        \DateTime $startDate,
+        \DateTime $endDate,
         array $customSearch,
         array $indexTypes,
         bool $emptyPreResult,

Classes/Controller/CalendarController.php
@@ -416,13 +416,13 @@ class CalendarController extends AbstractController
         $indicies = [];
 
         // prepare selection
-        $selection = [];
+        $selections = [];
         $configurations = $this->getCurrentConfigurations();
         foreach (GeneralUtility::trimExplode(',', $this->settings['singleItems']) as $item) {
             list($table, $uid) = BackendUtility::splitTable_Uid($item);
             foreach ($configurations as $configuration) {
                 if ($configuration['tableName'] === $table) {
-                    $selection[] = [
+                    $selections[] = [
                         'configuration' => $configuration,
                         'uid' => $uid,
                     ];
@@ -432,7 +432,7 @@ class CalendarController extends AbstractController
         }
 
         // fetch index
-        foreach ($selection as $selection) {
+        foreach ($selections as $selection) {
             $this->indexRepository->setIndexTypes([$selection['configuration']['uniqueRegisterKey']]);
             $dummyIndex = new Index();
             $dummyIndex->setForeignTable($selection['configuration']['tableName']);

Classes/Domain/Model/PluginConfiguration.php
@@ -104,7 +104,7 @@ class PluginConfiguration extends AbstractModel
      */
     public function __construct()
     {
-        $this->categories = new \TYPO3\CMS\Extbase\Persistence\ObjectStorage();
+        $this->categories = new ObjectStorage();
     }
 
     /**
@@ -313,7 +313,7 @@ class PluginConfiguration extends AbstractModel
     /**
      * @param \TYPO3\CMS\Extbase\Persistence\ObjectStorage $categories
      */
-    public function setCategories(\TYPO3\CMS\Extbase\Persistence\ObjectStorage $categories)
+    public function setCategories(ObjectStorage $categories)
     {
         $this->categories = $categories;
     }

Classes/Domain/Repository/IndexRepository.php
@@ -171,7 +171,6 @@ class IndexRepository extends AbstractRepository
         //Get actual datetime
         $now = DateTimeUtility::getNow()->getTimestamp();
 
-        $constraints = [];
         $constraints = $this->getDefaultConstraints($query);
         $constraints[] = $query->lessThanOrEqual('startDate', $now);
         $sort = QueryInterface::ORDER_ASCENDING === $sort ? QueryInterface::ORDER_ASCENDING : QueryInterface::ORDER_DESCENDING;

Classes/Service/TimeTableService.php
@@ -42,10 +42,11 @@ class TimeTableService extends AbstractService
                 continue;
             }
 
-            $handler = $this->buildConfigurationHandler($configuration);
-            if (!$handler) {
+            try {
+                $handler = $this->buildConfigurationHandler($configuration);
+            }catch(\Exception $exception){
                 HelperUtility::createFlashMessage(
-                    'There is no TimeTable handler for the given configuration type: ' . $configuration->getType(),
+                    $exception->getMessage(),
                     'Index invalid',
                     FlashMessage::ERROR
                 );
@@ -189,15 +190,15 @@ class TimeTableService extends AbstractService
      *
      * @param Configuration $configuration
      *
-     * @return bool|AbstractTimeTable
+     * @return AbstractTimeTable
+     * @throws Exception
      */
-    protected function buildConfigurationHandler(Configuration $configuration)
+    protected function buildConfigurationHandler(Configuration $configuration):AbstractTimeTable
     {
         $handler = 'HDNET\\Calendarize\\Service\\TimeTable\\' . \ucfirst($configuration->getType()) . 'TimeTable';
         if (!\class_exists($handler)) {
-            return false;
+            throw new \Exception('There is no TimeTable handler for the given configuration type: ' . $configuration->getType(), 1236781);
         }
-
         return HelperUtility::create($handler);
     }
 }

Classes/Service/Url/AbstractUrl.php
@@ -33,7 +33,7 @@ abstract class AbstractUrl extends AbstractService
      *
      * @return string
      */
-    protected function getIndexBase($indexUid)
+    protected function getIndexBase($indexUid):string
     {
         $indexRepository = HelperUtility::create(IndexRepository::class);
         $index = $indexRepository->findByUid((int) $indexUid);
@@ -53,6 +53,6 @@ abstract class AbstractUrl extends AbstractService
                 ->format($datePart);
         }
 
-        return $base;
+        return (string)$base;
     }
 }

Classes/Service/Url/RealUrl.php
@@ -76,8 +76,10 @@ WHERE tx_calendarize_domain_model_index.uid IS NULL AND tx_realurl_uniqalias.tab
      * Handle the alias to index ID convert.
      *
      * @param $value
+     *
+     * @return int
      */
-    protected function alias2id($value)
+    protected function alias2id($value):int
     {
         $databaseConnection = HelperUtility::getDatabaseConnection();
         $row = $databaseConnection->exec_SELECTgetSingleRow(
@@ -97,8 +99,9 @@ WHERE tx_calendarize_domain_model_index.uid IS NULL AND tx_realurl_uniqalias.tab
 
         $matches = [];
         if (\preg_match('/^idx-([0-9]+)$/', $value, $matches)) {
-            return $matches[1];
+            return (int)$matches[1];
         }
+        return 0;
     }
 
     /**
@@ -108,7 +111,7 @@ WHERE tx_calendarize_domain_model_index.uid IS NULL AND tx_realurl_uniqalias.tab
      *
      * @return string
      */
-    protected function id2alias($value)
+    protected function id2alias($value):string
     {
         $databaseConnection = HelperUtility::getDatabaseConnection();
         $row = $databaseConnection->exec_SELECTgetSingleRow(
@@ -120,7 +123,7 @@ WHERE tx_calendarize_domain_model_index.uid IS NULL AND tx_realurl_uniqalias.tab
             ) . ' AND value_id=' . (int) $value
         );
         if (isset($row['value_alias'])) {
-            return $row['value_alias'];
+            return (string)$row['value_alias'];
         }
 
         $alias = $this->getIndexBase((int) $value);
@@ -148,7 +151,7 @@ WHERE tx_calendarize_domain_model_index.uid IS NULL AND tx_realurl_uniqalias.tab
         }
         $databaseConnection->exec_INSERTquery('tx_realurl_uniqalias', $entry);
 
-        return $alias;
+        return (string)$alias;
     }
 
     /**
@@ -172,7 +175,7 @@ WHERE tx_calendarize_domain_model_index.uid IS NULL AND tx_realurl_uniqalias.tab
      *
      * @return string
      */
-    protected function cleanUrl($alias)
+    protected function cleanUrl(string $alias):string
     {
         if ($this->isOldRealUrlVersion()) {
             /** @var \tx_realurl_advanced $realUrl */
@@ -190,7 +193,7 @@ WHERE tx_calendarize_domain_model_index.uid IS NULL AND tx_realurl_uniqalias.tab
             $processedTitle = $utility->convertToSafeString($alias);
         }
 
-        return $processedTitle;
+        return (string)$processedTitle;
     }
 
     /**

commit 52329c87c2635cf1ded338a99dab76ecf57e6c06

Fix #260 - Define default values for all arguments
Bug introduced in 68f90b8329015bcbc69ba30e946545a0558e6187

Classes/Slots/EventSearch.php
@@ -48,12 +48,12 @@ class EventSearch
      */
     public function setIdsByCustomSearch(
         array $indexIds,
-        \DateTime $startDate,
-        \DateTime $endDate,
-        array $customSearch,
-        array $indexTypes,
-        bool $emptyPreResult,
-        array $additionalSlotArguments
+        \DateTime $startDate = null,
+        \DateTime $endDate = null,
+        array $customSearch = [],
+        array $indexTypes = [],
+        bool $emptyPreResult = false,
+        array $additionalSlotArguments = []
     ) {
         if (!\in_array('Event', $indexTypes, true)) {
             return;