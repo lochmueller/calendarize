05e995fabcebc635d90d1dd0f72f3ce6a9b1e7e2
Tim Lochmüller <tim@fruit-lab.de>
2020-04-22 20:23:29 +0200

    Fix version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,8 +15,8 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.13-9.5.99',
-            'php' => '7.2.0-0.0.0',
-            'autoloader' => '6.0.0-0.0.0',
+            'php' => '7.2.0-7.4.99',
+            'autoloader' => '6.0.0-6.99.99',
         ],
     ],
     'autoload' => [
