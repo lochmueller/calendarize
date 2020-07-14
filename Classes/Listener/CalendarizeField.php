b026a7f07be584b094194659daed769dc32cfc88
Daniel Haupt <dh@schiffel.it>
2020-07-14 21:07:13 +0200

    [BUGFIX] Migrate Slot 'Database' to PSR-14 event listener 'CalendarizeField'

---
+++ Classes/Listener/CalendarizeField.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace HDNET\Calendarize\Listener;
+
+use HDNET\Calendarize\Register;
+use TYPO3\CMS\Core\Database\Event\AlterTableDefinitionStatementsEvent;
+
+class CalendarizeField
+{
+    public function __invoke(AlterTableDefinitionStatementsEvent $event): void
+    {
+        $event->addSqlData($this->getCalendarizeDatabaseString());
+    }
+
+    /**
+     * Get the calendarize string for the registered tables.
+     *
+     * @return string
+     */
+    protected function getCalendarizeDatabaseString()
+    {
+        $sql = [];
+        foreach (Register::getRegister() as $configuration) {
+            $sql[] = 'CREATE TABLE ' . $configuration['tableName'] . ' (calendarize tinytext);';
+        }
+
+        return \implode(LF, $sql);
+    }
+}
