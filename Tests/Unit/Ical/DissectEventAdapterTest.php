6b10011b22536c78dc6f15f1a870a308721c2ad5
okmiim <okmiim@live.de>
2020-10-26 22:04:54 +0100

    Add ICalEvent unit tests

---
+++ Tests/Unit/Ical/DissectEventAdapterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HDNET\Calendarize\Tests\Unit\Ical;
+
+use HDNET\Calendarize\Ical\DissectEventAdapter;
+use HDNET\Calendarize\Ical\ICalEvent;
+use JMBTechnologyLimited\ICalDissect\ICalParser;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class DissectEventAdapterTest extends ICalEventTest
+{
+    protected $tmpFile;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->tmpFile = null;
+    }
+
+    protected function tearDown(): void
+    {
+        if (is_scalar($this->tmpFile) && file_exists($this->tmpFile)) {
+            unlink($this->tmpFile);
+        }
+        parent::tearDown();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getEvent(string $content): ICalEvent
+    {
+        $this->tmpFile = GeneralUtility::tempnam('ical');
+        GeneralUtility::writeFile($this->tmpFile, $content);
+        $parser = new ICalParser();
+        if (!$parser->parseFromFile($this->tmpFile)) {
+            self::fail('Unable to open or parse temporary ical file.');
+        }
+        return new DissectEventAdapter($parser->getEvents()[0]);
+    }
+
+    public function testDuration()
+    {
+        self::markTestSkipped('The DissectEventAdapter does not support DURATION.');
+        parent::testDuration();
+    }
+}
---
+++ Tests/Unit/Ical/VObjectEventAdapterTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HDNET\Calendarize\Tests\Unit\Ical;
+
+use HDNET\Calendarize\Ical\ICalEvent;
+use HDNET\Calendarize\Ical\VObjectEventAdapter;
+use Sabre\VObject\Reader;
+
+class VObjectEventAdapterTest extends ICalEventTest
+{
+    protected function getEvent(string $content): ICalEvent
+    {
+        $vcalendar = Reader::read($content);
+
+        /* @var \Sabre\VObject\Component\VEvent $vevent */
+        $vevent = $vcalendar->VEVENT;
+        return new VObjectEventAdapter($vevent);
+    }
+}
