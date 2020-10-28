136987ee2a200147cd30518e7b989f39b683c8f2
okmiim <okmiim@live.de>
2020-10-28 21:34:00 +0100

    Replace ics service in ExternalTimeTable

--- Classes/Service/TimeTable/ExternalTimeTable.php
+++ Classes/Service/TimeTable/ExternalTimeTable.php
@@ -8,7 +8,8 @@ declare(strict_types=1);
 namespace HDNET\Calendarize\Service\TimeTable;
 
 use HDNET\Calendarize\Domain\Model\Configuration;
-use HDNET\Calendarize\Service\IcsReaderService;
+use HDNET\Calendarize\Service\Ical\ICalServiceInterface;
+use HDNET\Calendarize\Utility\DateTimeUtility;
 use HDNET\Calendarize\Utility\HelperUtility;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -18,6 +19,23 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  */
 class ExternalTimeTable extends AbstractTimeTable
 {
+
+    /**
+     * Ical service.
+     *
+     * @var ICalServiceInterface
+     */
+    protected $iCalService;
+
+    /**
+     * Inject ical service.
+     *
+     * @param ICalServiceInterface $iCalService
+     */
+    public function injectICalServiceInterface(ICalServiceInterface $iCalService)
+    {
+        $this->iCalService = $iCalService;
+    }
 
     /**
      * Modify the given times via the configuration.
@@ -40,12 +58,55 @@ class ExternalTimeTable extends AbstractTimeTable
             return;
         }
 
-        $icsReaderService = GeneralUtility::makeInstance(IcsReaderService::class);
-        $externalTimes = $icsReaderService->getTimes($url);
-        foreach ($externalTimes as $time) {
+        $fileName = $this->getCachedUrlFile($url);
+        $externalTimes = $this->iCalService->getEvents($fileName);
+
+        foreach ($externalTimes as $event) {
+            $time = [
+                'start_date' => $event->getStartDate(),
+                'end_date' => $event->getEndDate(),
+                'start_time' => $event->getStartTime(),
+                'end_time' => $event->getEndTime(),
+                'all_day' => $event->isAllDay(),
+                'open_end_time' => $event->isOpenEndTime(),
+                'state' => $event->getState(),
+            ];
             $time['pid'] = $configuration->getPid();
             $time['state'] = $configuration->getState();
             $times[$this->calculateEntryKey($time)] = $time;
         }
+    }
+
+    /**
+     * Get cached URL file.
+     *
+     * @param string $url
+     *
+     * @return string
+     */
+    protected function getCachedUrlFile(string $url): string
+    {
+        $tempFileName = $this->getCheckedCacheFolder() . \md5($url);
+        if (!\is_file($tempFileName) || \filemtime($tempFileName) < (\time() - DateTimeUtility::SECONDS_HOUR)) {
+            $icsFile = GeneralUtility::getUrl($url);
+            GeneralUtility::writeFile($tempFileName, $icsFile);
+        }
+
+        return $tempFileName;
+    }
+
+    /**
+     * Return the cache folder and check if the folder exists.
+     *
+     * @return string
+     */
+    protected function getCheckedCacheFolder(): string
+    {
+        $cacheFolder = GeneralUtility::getFileAbsFileName('typo3temp/var/transient/calendarize/');
+        if (!\is_dir($cacheFolder)) {
+            GeneralUtility::mkdir_deep($cacheFolder);
+        }
+
+        return $cacheFolder;
     }
 }
