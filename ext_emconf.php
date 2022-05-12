fc0075d94aed7c43f2c9699b4f11137396e2c9a6
Tim <tim@fruit-lab.de>
2022-05-12 21:57:57 +0200

    Fix #344 - Get ext_emconf.php and composer.json in sync - PHP ^8.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.99',
-            'php' => '7.4.0-8.0.99',
+            'php' => '7.4.0-8.99.99',
             'autoloader' => '7.1.0-7.99.99',
         ],
         'suggests' => [
