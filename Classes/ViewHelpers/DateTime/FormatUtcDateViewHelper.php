117a30ded83069eae1968ee63f192f8a9613807b
Imko Schumacher <okmiim@live.de>
2023-10-20 17:34:32 +0200

    Fix format UTC ViewHelper

--- Classes/ViewHelpers/DateTime/FormatUtcDateViewHelper.php
+++ Classes/ViewHelpers/DateTime/FormatUtcDateViewHelper.php
@@ -4,47 +4,15 @@ declare(strict_types=1);
 
 namespace HDNET\Calendarize\ViewHelpers\DateTime;
 
+use HDNET\Calendarize\ViewHelpers\Format\DateViewHelper;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
-use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Exception;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithContentArgumentAndRenderStatic;
 
 /**
  * Formats the date to UTC.
  */
-class FormatUtcDateViewHelper extends AbstractViewHelper
+class FormatUtcDateViewHelper extends DateViewHelper
 {
-    use CompileWithContentArgumentAndRenderStatic;
-
-    /**
-     * Needed as child node's output can return a DateTime object which can't be escaped.
-     *
-     * @var bool
-     */
-    protected $escapeChildren = false;
-
-    public function initializeArguments(): void
-    {
-        $this->registerArgument(
-            'date',
-            'mixed',
-            'Either an object implementing DateTimeInterface or a string that is accepted by DateTime constructor'
-        );
-        $this->registerArgument(
-            'format',
-            'string',
-            'Format String which is taken to format the Date/Time',
-            false,
-            ''
-        );
-        $this->registerArgument(
-            'base',
-            'mixed',
-            'A base time (an object implementing DateTimeInterface or a string) used if $date is a relative
-             date specification. Defaults to current time.'
-        );
-    }
-
     /**
      * Format dateTime to the UTC timezone.
      *
@@ -56,7 +24,8 @@ class FormatUtcDateViewHelper extends AbstractViewHelper
         array $arguments,
         \Closure $renderChildrenClosure,
         RenderingContextInterface $renderingContext
-    ) {
+    ): string
+    {
         // save configured timezone
         $timezone = date_default_timezone_get();
         // set timezone to UTC
