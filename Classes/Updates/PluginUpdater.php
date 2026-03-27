b1ab0e6d7e67cd11e0b02418808470681a765245
Ruben Silva <ruben@hemmer.ch>
2026-03-27 10:04:28 +0100

    [TASK] Import upgrade wizard attribute from core since it's been moved
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/14.0/Deprecation-106947-MoveUpgradeWizardRelatedInterfacesAndAttributeToEXTcore.html#deprecation-106947-move-upgrade-wizard-related-interfaces-and-attribute-to-ext-core

--- Classes/Updates/PluginUpdater.php
+++ Classes/Updates/PluginUpdater.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace HDNET\Calendarize\Updates;
 
+use TYPO3\CMS\Core\Attribute\UpgradeWizard;
 use TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Service\FlexFormService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Install\Attribute\UpgradeWizard;
 use TYPO3\CMS\Install\Updates\DatabaseUpdatedPrerequisite;
 
 #[UpgradeWizard('calendarize_pluginUpdater')]
